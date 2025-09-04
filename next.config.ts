92a1d67d53115b26db80ccd3e396ee666e71844f
unknown <rbear.co@gmail.com>
2025-09-04 11:08:26 +0330

    fix next.config.ts for vercel images

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
+    domains: ["randomuser.me"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "randomuser.me",
-        port: "",
         pathname: "/**",
       },
     ],
